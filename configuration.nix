16dc9977f5ebf2992272fbec050be27c4b15541e
Trenton Fox <tcfox54@gmail.com>
2025-02-23 16:43:55 -0600

    Removed Steam's withJava
    It doens't work.

--- configuration.nix
+++ configuration.nix
@@ -263,7 +263,7 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    package = pkgs.steam.override { withJava = true; };
+    #package = pkgs.steam.override { withJava = true; };
      extraCompatPackages = [
        pkgs.proton-ge-bin
      ];
